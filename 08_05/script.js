b30d9bb5554bb74c5ccd48b41986732b18468c1e
Brian Grant <brian@skeletonjack.net>
2023-01-31 09:50:50 -0600

    functions & methods up to Practice

--- 08_05/script.js
+++ 08_05/script.js
@@ -13,12 +13,18 @@ const greenPack = {
   volume: 8,
   pocketNum: 3,
   newVolume: function (volume) {
+    // methods cannot be declared using arrow functions
     console.log("this.volume in the method:", this.volume);
     this.volume = volume;
     console.log("this.volume after update:", this.volume);
     // (function () {
+    //   // the function declaration results in this function being hoisted to the global scope where `this` refers to `window`
     //   console.log("this.volume in nested function:", this.volume);
     // })();
+    (() => {
+      // the arrow function declaration is scoped to the block in which it's declared
+      console.log("this.volume in nested function:", this.volume);
+    })();
   },
 };
 
